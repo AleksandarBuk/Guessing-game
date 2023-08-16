1cafcac5149b129ec164a4a74f0ffb2de6e0c63d
AleksandarBuk <aleksandar.buk@gmail.com>
2023-08-16 23:41:49 +0200

    Updated game logic

--- App.py
+++ App.py
@@ -9,7 +9,13 @@ def run_guess(guess, answer, attempts):
     counter = attempts
     if 0 < guess < 11:
         if guess == answer:
-            print('You got the correct answer in', attempts, 'attempts! You\'re a genius!')
+            if attempts > 3:
+                print('You got the correct answer in', attempts, 'attempts! Well done!')
+            elif attempts <= 2:
+                print('You got the correct answer in', attempts, 'attempts! You\'re a genius!')
+            else:
+                print('You got the correct answer in', attempts, 'attempts! Well done!')
+
             return True
         elif counter == 2:
             print(f'that\'s not the correct answer. Here\'s a clue: ', clue(answer))
@@ -34,7 +40,7 @@ if __name__ =='__main__':
                 continue
 
         play_again_input = input('Do you want to give it another try?(yes/no): ')
-        if play_again_input.lower() != 'yes':
+        if play_again_input.lower() not in ['yes', 'y']:
             print('Come back again! Bye!!!')
             play_again = False
 
