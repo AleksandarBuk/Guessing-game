f92359e880387df8b5c0ccf5d750565a8355fce8
AleksandarBuk <aleksandar.buk@gmail.com>
2023-08-18 07:25:23 +0200

    Defined player, added @click

--- App.py
+++ App.py
@@ -1,29 +1,52 @@
 from random import randint, choice
 
+
+class Player:
+
+    def __init__(self):
+        self.name = None
+        # self.points = points
+
+    def name_get(self):
+        self.name = input(f'Set your username: ')
+
+    def name_print(self):
+        print(f'Username set to: ', self.name)
+
+    # create name_display function
+
+
 def clue(answer):
-    options = [answer - 1, answer +1]
+    options = [answer - 1, answer + 1]
     return choice(options)
 
-def run_guess(guess, answer, attempts):
 
-    counter = attempts
-    if 0 < guess < 11:
-        if guess == answer:
-            if attempts > 3:
-                print('You got the correct answer in', attempts, 'attempts! Well done!')
-            elif attempts <= 2:
-                print('You got the correct answer in', attempts, 'attempts! You\'re a genius!')
+
+def run_guess(user_guess, correct_answer, attempt):
+    counter = attempt
+    if 0 < user_guess < 11:
+        if user_guess == correct_answer:
+            if attempt > 3:
+                print('You got the correct answer in', attempt, 'attempts! Well done!')
+            elif attempt <= 2:
+                print('You got the correct answer in', attempt, 'attempts! You\'re a genius!')
             else:
-                print('You got the correct answer in', attempts, 'attempts! Well done!')
+                print('You got the correct answer in', attempt, 'attempts! Well done!')
 
             return True
         elif counter == 2:
-            print(f'that\'s not the correct answer. Here\'s a clue: ', clue(answer))
+            print(f'that\'s not the correct answer. Here\'s a clue: ', clue(correct_answer))
     else:
         print('Please input a number that is 1~10')
         return False
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
+
+    player = Player()
+    player.name_get()
+    player.name_print()
+
     play_again = True
     while play_again:
         answer = randint(1, 10)
@@ -32,7 +55,7 @@ if __name__ =='__main__':
             try:
                 guess = int(input('Please input a number 1~10: '))
                 attempts += 1
-                if run_guess(guess, answer, attempts):
+                if run_guess(guess, answer, attempts): #error to fix
                     break
 
             except ValueError:
@@ -44,4 +67,4 @@ if __name__ =='__main__':
             print('Come back again! Bye!!!')
             play_again = False
 
-# add score tracker
+    # add score tracker
